ab7b174f8b151034dab2e9b3d8c86a918f2afb20
Sudharshan1904 <sudhankumar1904@gmail.com>
2026-01-12 16:40:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import Main from "./components/Main";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Project from "./pages/Project";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
       <Sidebar />
+
       <div className="main">
         <Routes>
+          {/* Default Home Page */}
           <Route path="/" element={<Main />} />
+
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="/project" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+
+          {/* Fallback: redirect any unknown route to Home */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </div>
